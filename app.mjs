e12482caa0e8500330b7083557697961060676dc
Emile Nijssen <emile@emile.nl>
2025-08-19 13:07:01 +0200

    wip

--- app.mjs
+++ app.mjs
@@ -1,15 +1,2 @@
-import Homey from 'homey';
-import MatterServer from './lib/MatterServer.mjs'
-
-export default class MatterBridgeApp extends Homey.App {
-
-  async onInit() {
-    this.log('Starting Matter Server...');
-
-    this.server = new MatterServer();
-    await this.server.start({
-      uniqueId: 'abcdef', // await this.homey.cloud.getHomeyId(),
-    });
-  }
-
-}
+import MatterBridgeApp from './lib/MatterBridgeApp.mjs';
+export default MatterBridgeApp;
